234b2a7af1d2b9ed733058f0256e9a46c72f4f11
DESKTOP-IGHA7RH\Adrian Gamarra <adriatogi@gmail.com>
2020-04-21 12:17:10 -0700

    Initial Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void loadLevel1()
+    {
+        SceneManager.LoadScene("Level_1"); 
+    }
+    public void loadLevel2()
+    {
+        SceneManager.LoadScene("Level_2"); 
+    }
+}
