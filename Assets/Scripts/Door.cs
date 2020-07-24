3fccaf967ba6e3d8c608aadd0d5a96d6a363ddcc
DESKTOP-IGHA7RH\Adrian Gamarra <adriatogi@gmail.com>
2020-07-23 17:41:10 -0700

    Finish Level Functionality Pt. 1

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite _doorClose;
+
+    private SpriteRenderer _spriteRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+    private void openDoor()
+    {
+        _spriteRenderer.sprite = _doorClose;
+    }
+
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -16,12 +16,6 @@ public class UIManager : MonoBehaviour
     private GameObject _pausePanel;
     private bool _isPaused = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
